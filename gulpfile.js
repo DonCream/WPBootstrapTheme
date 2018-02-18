e666716b56a39660fa54d2589e5377e0b0476303
cream <lamar.mcmiller@gmail.com>
2018-02-18 01:05:48 -0500

    Update to working file
    last commit wasnt working properly

--- gulpfile.js
+++ gulpfile.js
@@ -12,59 +12,43 @@ const cache = require('gulp-cache');
 const htmlsplit = require('gulp-htmlsplit');
 
 var reload = browserSync.reload;
-var src = {
-                css: 'src/css/*.css',
-                scss: 'src/scss/*.scss',
-                images: 'src/images/*.*',
-                js: 'src/js/*.js',
-                html: 'src/*.html'
-};
-var dist = {
-                css: 'dist/css/*.css',
-                scss: 'dist/scss/*.scss',
-                images: 'dist/images/*.*',
-                js: 'dist/js/*.js',
-                html: 'dist/*.php'
-};
 
-// Add minified Bootstrap to the src/css folder
-gulp.task('boots', function() {
-   return gulp.src(['node_modules/bootstrap/dist/css/bootstrap.css'])
-      .pipe(gulp.dest("src/css"))
-      .pipe(browserSync.stream());
-});
-// Move JS Files to src/js
-gulp.task('js', function() {
-   return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
-      .pipe(gulp.dest("src/js"))
-      .pipe(browserSync.stream());
-});
-// Move Fonts Folder to src/fonts
-gulp.task('fonts', function() {
-   return gulp.src('node_modules/font-awesome/fonts/*')
-      .pipe(gulp.dest("src/fonts"));
-});
-// Move Font Awesome CSS to src/css
-gulp.task('fa', function() {
-   return gulp.src('node_modules/font-awesome/css/font-awesome.min.css')
-      .pipe(gulp.dest("src/css"));
-});
 // Compile Sass & Inject Into Browser
-gulp.task('sass', function() {
-   return gulp.src(src.scss)
-      .pipe(sass().on('error', sass.logError))
-      .pipe(gulp.dest('src/css'))
-      .pipe(reload({stream: true}));
-});
-// Watch Sass & Server
-gulp.task('serve', ['sass'], function() {
-   browserSync.init({
-      server: "./src"
-   });
-   gulp.watch(src.scss, ['sass']);
-   gulp.watch(src.html).on('change', reload);
+gulp.task('sass', function(){
+  return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss','src/scss/*.scss'])
+    .pipe(sass())
+    .pipe(gulp.dest("src/css"))
+    .pipe(browserSync.stream());
 });
 
+// Move JS Files to src/js
+gulp.task('js', function(){
+  return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js','node_modules/jquery/dist/jquery.min.js','node_modules/popper.js/dist/umd/popper.min.js'])
+    .pipe(gulp.dest("src/js"))
+    .pipe(browserSync.stream());
+});
+
+// Watch Sass & Server
+gulp.task('serve', ['sass'], function(){
+  browserSync.init({
+    server: "./src"
+  });
+
+  gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], ['sass']);
+  gulp.watch("src/*.html").on('change', browserSync.reload);
+});
+
+// Move Fonts Folder to src/fonts
+gulp.task('fonts', function(){
+  return gulp.src('node_modules/font-awesome/fonts/*')
+    .pipe(gulp.dest("src/fonts"));
+});
+
+// Move Font Awesome CSS to src/css
+gulp.task('fa', function(){
+  return gulp.src('node_modules/font-awesome/css/font-awesome.min.css')
+    .pipe(gulp.dest("src/css"));
+});
 //Inject Wordpress theme header and move to /src
 gulp.task('split', function() {
   gulp.src('./src/*.html')
