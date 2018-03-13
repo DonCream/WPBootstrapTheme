f47713dab60491d5988b5da2b92ab6d1ba559cbb
cream <lamar.mcmiller@gmail.com>
2018-03-13 03:26:08 -0400

    Perfected my gulpfile!
    got it to inject WP theme heading, split html files into php files, and add php header and footer tags to .php files

--- gulpfile.js
+++ gulpfile.js
@@ -7,35 +7,18 @@ const rename = require('gulp-rename');
 const browserSync = require('browser-sync').create();
 const autoprefixer = require('gulp-autoprefixer');
 const imagemin = require('gulp-imagemin');
-const inject = require('gulp-inject');
 const cache = require('gulp-cache');
 const htmlsplit = require('gulp-htmlsplit');
+const inject = require('gulp-inject-string');
+const processhtml = require('gulp-processhtml');
 
-var reload = browserSync.reload;
+var reload = browserSync.reload,
+opts = { /* plugin options */ };
 
-// Compile Sass & Inject Into Browser
-gulp.task('sass', function(){
-  return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss','src/scss/*.scss'])
-    .pipe(sass())
-    .pipe(gulp.dest("src/css"))
-    .pipe(browserSync.stream());
-});
-
-// Move JS Files to src/js
-gulp.task('js', function(){
-  return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js','node_modules/jquery/dist/jquery.min.js','node_modules/popper.js/dist/umd/popper.min.js'])
-    .pipe(gulp.dest("src/js"))
-    .pipe(browserSync.stream());
-});
-
-// Watch Sass & Server
-gulp.task('serve', ['sass'], function(){
-  browserSync.init({
-    server: "./src"
-  });
-
-  gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], ['sass']);
-  gulp.watch("src/*.html").on('change', browserSync.reload);
+// Move Bootstrap CSS to src/css
+gulp.task('boots', function(){
+  return gulp.src('node_modules/bootstrap/dist/css/bootstrap.css')
+    .pipe(gulp.dest("src/css"));
 });
 
 // Move Fonts Folder to src/fonts
@@ -50,25 +33,92 @@ gulp.task('fa', function(){
     .pipe(gulp.dest("src/css"));
 });
 
-//Inject Wordpress theme header and move to /dist
+// Move particles.js to src/js
+gulp.task('particles', function(){
+  return gulp.src('node_modules/particlesjs/dist/particles.min.js')
+    .pipe(gulp.dest("src/js"));
+});
+
+// Move Lightbox js to src/js
+gulp.task('ekkojs', function(){
+  return gulp.src('node_modules/ekko-lightbox/dist/ekko-lightbox.min.js')
+    .pipe(gulp.dest("src/js"));
+});
+
+// Move Lightbox css to src/css
+gulp.task('ekkocss', function(){
+  return gulp.src('node_modules/ekko-lightbox/dist/ekko-lightbox.min.css')
+    .pipe(gulp.dest("src/css"));
+});
+
+// Move JS Files to src/js
+gulp.task('js', function(){
+  return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js','node_modules/jquery/dist/jquery.min.js','node_modules/popper.js/dist/umd/popper.min.js'])
+    .pipe(gulp.dest("src/js"))
+    .pipe(browserSync.stream());
+});
+
+// Compile Sass & Inject Into Browser
+gulp.task('sass', function(){
+  return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss','src/scss/*.scss'])
+    .pipe(sass())
+    .pipe(gulp.dest("src/css"))
+    .pipe(browserSync.stream());
+});
+
+
+// Watch Sass & Server
+gulp.task('serve', ['sass'], function(){
+  browserSync.init({
+    server: "./src"
+  });
+
+  gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], ['sass']);
+  gulp.watch("src/*.html").on('change', browserSync.reload);
+});
+
+// add php tags to .php files
+gulp.task('process', function () {
+    return gulp.src('src/*.html')
+               .pipe(processhtml(opts))
+               .pipe(gulp.dest('src'));
+});
+
+//Split into .php files and move to /dist
 gulp.task('split', function() {
-  gulp.src('./src/*.html')
+  gulp.src('src/*.html')
     .pipe(htmlsplit())
     .pipe(gulp.dest('dist'));
 });
 
-//Move src/bootstrap.css to /dist/css
+//Move style.css to /dist and add Wordpress header
+gulp.task('addhdft', function() {
+   gulp.src(['dist/*.php', '!dist/header.php', '!dist/footer.php'])
+      .pipe(inject.prepend('<?php get_header(); ?>\n'))
+      .pipe(inject.append('\n<?php get_footer(); ?>'))
+      .pipe(gulp.dest('dist'))
+      .pipe(browserSync.stream());
+});
+
+//Move style.css to /dist and add Wordpress header
+gulp.task('cssmain', function() {
+   gulp.src('src/css/style.css')
+      .pipe(inject.prepend('/* Theme Name: Corners Theme Theme URI: Author: Lamar McMiller Author URI: http://www.lamarmcmiller.me Description: Bootstrap 4 Worpress Theme Version: 1.0 License: GNU General Public License v2 or later License URI: http://www.gnu.org/licenses/gpl-2.0.html Text Domain: */\n'))
+      .pipe(gulp.dest('dist'))
+      .pipe(browserSync.stream());
+});
+
+//Move src/.css to /dist/css
 gulp.task('cssmv', function() {
-   gulp.src(['src/css/bootstrap.css', 'src/css/bootstrap.min.css'])
-      .pipe(concat('bootstrap.min.css'))
+   gulp.src('src/css/*.css')
       .pipe(gulp.dest('dist/css'))
       .pipe(browserSync.stream());
 });
 
-//Move src/style.css to /dist
-gulp.task('famv', function() {
-   gulp.src('src/css/font-awesome.min.css')
-      .pipe(gulp.dest('dist/css'))
+//Move .html files to /dist and rename to .php
+gulp.task('htmlmv', function() {
+   gulp.src('src/*.html')
+      .pipe(gulp.dest('dist'))
       .pipe(browserSync.stream());
 });
 
@@ -90,7 +140,7 @@ gulp.task('imageMin', () =>
    .pipe(gulp.dest('dist/images'))
 );
 
-// concatenate .js files into main.js and move to /dist/js
+// Move .js files to /dist/js
 gulp.task('scripts', function() {
    gulp.src('src/js/*.js')
       .pipe(gulp.dest('dist/js'))
@@ -108,4 +158,5 @@ gulp.task('php', ['sass'], function() {
 
 gulp.task('default', ['boots', 'js', 'fa', 'fonts', 'sass']);
 gulp.task('srv', ['serve']);
-gulp.task('prod', ['scripts', 'famv', 'imageMin', 'fontsmv', 'cssmv']);
+gulp.task('prod', ['process', 'split', 'scripts', 'imageMin', 'fontsmv', 'cssmain', 'cssmv']);
+
